6c21a958cd50b2e9e1fa3489c04a3c720f8283bd
Nicholas Schreiber <rnd7@riseup.net>
2018-10-25 10:20:40 +0200

    Automatic Commit

--- bin/publish-rum.js
+++ bin/publish-rum.js
@@ -31,8 +31,8 @@ const queue = [
   [true, 'git', ['commit', '-m', comment]],
   [true, 'npm', ['version', version]],
   [build, 'npx make-rum', []],
-  [true, 'git', ['add', '--all']],
-  [true, 'git', ['commit', '-m', function() {
+  [build, 'git', ['add', '--all']],
+  [build, 'git', ['commit', '-m', function() {
     return JSON.parse(fs.readFileSync('package.json')).version
   }]]
 ]
